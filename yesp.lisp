9ca1130a97d5dc58b62121b5884859e034ea613b
Benjámin J. Ferge <benjamin.ferge@protonmail.com>
2020-06-22 15:24:10 +0200

    Load DB from files

--- yesp.lisp
+++ yesp.lisp
@@ -29,24 +29,41 @@
 		       :if-exists :append
 		       :if-does-not-exist :create)
     (with-standard-io-syntax
-      (dolist (item (getf event-stream :events)) (print item out)))))
+      (dolist (item (getf event-stream :events)) (prin1 item out)))))
 
 (defun event-stream-files ()
   (list-directory *db-dir*))
 
-(defun load-event-stream (name)
-  name)
+(defun read-event-stream-file (path)
+  (with-open-file (in path)
+    (loop for form = (read in nil)
+       while form
+       collect form)))
 
-(defun save-db ()
-  (with-open-file (out *db-path*
-		       :direction :output
-		       :if-exists :append
-		       :if-does-not-exist :create)
-    (with-standard-io-syntax
-      (print *db* out))))
+(defun load-event-stream (event-stream)
+  (setf (gethash (getf event-stream :name) *db*) event-stream))
+
+(defun event-stream-from-file (path)
+  (make-event-stream
+   (read-from-string (pathname-name path))
+   (read-event-stream-file path)))
+
+(defun load-event-stream-from-file (path)
+  (load-event-stream (event-stream-from-file path)))
 
 (defun load-db ()
-  (dolist (f (event-stream-files))
-    (with-open-file (in f)
-      (with-standard-io-syntax
-	(print (read in))))))
+  (walk-directory *db-dir* #'load-event-stream-from-file))
+
+(defmethod print-object ((object hash-table) stream)
+  (format stream "#<HASH-TABLE :TEST ~a :COUNT ~a {~x}~%  ~{~{(~a : ~a)~}~^ ~}>"
+          (hash-table-test object)
+	  (hash-table-count object)
+	  #+sbcl
+	  (sb-kernel:get-lisp-obj-address object)
+	  #+clisp
+	  (system::address-of object)
+	  #-(or sbcl clisp)
+	  'not-implemented
+	  (loop for key being the hash-keys of object
+	     using (hash-value value)
+	     collect (list key value))))
