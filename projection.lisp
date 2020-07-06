d13bf682227a4c18dd21565542dcb6bc40d28885
Benjámin J. Ferge <benjamin.ferge@protonmail.com>
2020-07-06 07:14:57 +0200

    WIP on projections

--- projection.lisp
+++ projection.lisp
@@ -1,1 +1,18 @@
 (in-package #:yesp)
+
+(defclass projection ()
+  ((name
+    :initarg :name
+    :accessor name)
+   (body
+    :initarg :body
+    :accessor body)
+   (stream-id
+    :initarg :stream-id
+    :initform nil
+    :accessor stream-id)))
+
+(defmethod call ((p projection) &key :stream-name)
+  (let ((streams (gethash stream-name *db*)))
+    streams))
+
